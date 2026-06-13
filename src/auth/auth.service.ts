db2abe4d051bfc2dc6265bb07a2ddd01a822af95
Muhammad Tabish <tabishm484@gmail.com>
2026-06-13 14:05:55 +0500

    feat: add User module and service, integrate with Auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterDto } from './Dto/register.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
   register(registerDto: RegisterDto) {
+    this.userService.getUserByEmail(registerDto.email);
     return {
       message: registerDto,
     };
